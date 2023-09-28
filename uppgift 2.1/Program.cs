89803d0dd4a7eac0ddad5820c1bdb8fc7754bd55
Pingvu <nhlpool27@gmail.com>
2023-09-28 13:00:36 +0200

    Add project files.

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift_2_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt hoppade Elin i meter ?");
+            string HoppadeText = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hur långt hoppade Alma i meter ?");
+            string hoppadeText2 = Console.ReadLine();
+            Console.Clear();
+
+            double hoppade = double.Parse(HoppadeText);
+         
+            Console.WriteLine("2 meter - 3 meter = skillnaden är 1 meter");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
